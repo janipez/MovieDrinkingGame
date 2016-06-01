8d3ac82a81e998bb4b035a0a893881d4e311412d
JaNi <jani.pezdevsek@gmail.com>
2016-06-01 19:29:47 +0200

    Branje iz baze, custom adapter za class Film.

--- app/src/main/java/com/jadi/moviedrinkinggame/BazaPomocnik.java
+++ app/src/main/java/com/jadi/moviedrinkinggame/BazaPomocnik.java
@@ -2,14 +2,19 @@ package com.jadi.moviedrinkinggame;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by JaNi on 1. 06. 2016.
  */
 
-public class BazaPomocnik extends SQLiteOpenHelper {
+public class BazaPomocnik extends SQLiteOpenHelper
+{
 
     //Ime PB
     static final String PB_IME = "FILMI_IN_DOGODKI.DB";
@@ -36,32 +41,37 @@ public class BazaPomocnik extends SQLiteOpenHelper {
             "vrednost INTEGER, " +
             "casProzitve INTEGER, " +
             "tkIdFilm INTEGER, " +
-            "FOREIGN KEY(tkIdFilm) REFERENCES FILMI(idFilm)";
+            "FOREIGN KEY(tkIdFilm) REFERENCES FILMI(idFilm))";
 
-    public BazaPomocnik(Context context){
+    public BazaPomocnik(Context context)
+    {
         super(context, PB_IME, null, PB_VERZIJA);
     }
 
     //Ustvarjanje baze
     @Override
-    public void onCreate(SQLiteDatabase pb) {
+    public void onCreate(SQLiteDatabase pb)
+    {
         pb.execSQL(ustvariTabeloFilmi);
         pb.execSQL(ustvariTabeloDogodki);
     }
 
     //Nadgradnja baze
     @Override
-    public void onUpgrade(SQLiteDatabase pb, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase pb, int oldVersion, int newVersion)
+    {
         pb.execSQL("DROP TABLE IF EXISTS FILMI");
         pb.execSQL("DROP TABLE IF EXISTS DOGODKI");
         onCreate(pb);
     }
 
-    //Metoda za dodajanje filma
-    public void dodajFilm(Film film) {
+    //Dodajanje filma v bazo
+    public void dodajFilm(Film film)
+    {
         SQLiteDatabase pb = this.getWritableDatabase();
 
         ContentValues vrednosti = new ContentValues();
+        vrednosti.put("idFilm", film.getIdFilm());
         vrednosti.put("tip", film.getTip());
         vrednosti.put("naslov", film.getNaslov());
         vrednosti.put("zvrst", film.getZvrst());
@@ -73,6 +83,67 @@ public class BazaPomocnik extends SQLiteOpenHelper {
         pb.close();
     }
 
-    //Metoda za dodajanje dogodkov
+    //Branje 1 filma iz baze
+    public Film beriFilm(int id)
+    {
+        SQLiteDatabase pb = this.getWritableDatabase();
+        String selectStavek = "SELECT * FROM FILMI WHERE idFilm = " + String.valueOf(id);
+        Cursor cursor = pb.rawQuery(selectStavek, null);
+
+        Film film = new Film();
+
+        if (cursor.moveToFirst())
+        {
+            do
+            {
+                film.setIdFilm(Integer.parseInt(cursor.getString(0)));
+                film.setTip(cursor.getString(1));
+                film.setNaslov(cursor.getString(2));
+                film.setZvrst(cursor.getString(3));
+                film.setOcena(Integer.parseInt(cursor.getString(4)));
+                film.setCitat(cursor.getString(5));
+                film.setCasTrajanja(Integer.parseInt(cursor.getString(6)));
+            }
+            while (cursor.moveToNext());
+        }
+
+        pb.close();
+        cursor.close();
+
+        return film;
+    }
+
+    //Branje vseh filmov iz baze
+    public ArrayList<Film> beriVseFilme()
+    {
+        ArrayList<Film> seznamFilmov = new ArrayList<Film>();
+
+        SQLiteDatabase pb = this.getWritableDatabase();
+        String selectStavek = "SELECT * FROM FILMI";
+        Cursor cursor = pb.rawQuery(selectStavek, null);
+
+        if (cursor.moveToFirst())
+        {
+            do
+            {
+                Film film = new Film();
+
+                film.setIdFilm(Integer.parseInt(cursor.getString(0)));
+                film.setTip(cursor.getString(1));
+                film.setNaslov(cursor.getString(2));
+                film.setZvrst(cursor.getString(3));
+                film.setOcena(Integer.parseInt(cursor.getString(4)));
+                film.setCitat(cursor.getString(5));
+                film.setCasTrajanja(Integer.parseInt(cursor.getString(6)));
+
+                seznamFilmov.add(film);
+            }
+            while (cursor.moveToNext());
+        }
+
+        return seznamFilmov;
+    }
+
+    //Dodajanje dogodkov
 
 }
--- app/src/main/java/com/jadi/moviedrinkinggame/MainActivity.java
+++ app/src/main/java/com/jadi/moviedrinkinggame/MainActivity.java
@@ -2,12 +2,33 @@ package com.jadi.moviedrinkinggame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BazaPomocnik bp = new BazaPomocnik(this);
+
+        /*bp.dodajFilm(new Film(1, "film", "Avengers", "Science Fiction", 8, "Assemble!", 140));
+        bp.dodajFilm(new Film(2, "film", "Iron Man", "Science Fiction", 7, "Tony Stark was able to build this in a cave!", 130));
+        bp.dodajFilm(new Film(3, "film", "Shutter Island", "Mystery", 9, "Who is mad?", 120));
+        bp.dodajFilm(new Film(4, "serija", "Game of Thrones S02E05", "ZF", 9, "Hold the Door!", 55));
+        bp.dodajFilm(new Film(5, "serija", "Gotham S02E10", "ZF", 7, "Batman.", 44));*/
+
+        ArrayList<Film> vsiFilmi = bp.beriVseFilme();
+
+        FilmAdapter filmAdapter = new FilmAdapter(this, vsiFilmi);
+
+        ListView lv = (ListView) findViewById(R.id.listViewTest);
+        lv.setAdapter(filmAdapter);
     }
 }
